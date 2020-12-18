c7ed7bfcb91b1f546b4f81ebe4ee345c3d8cfdac
Felipe Ignacio Mancilla Reyes <felipei.mancilla@gmail.com>
2020-12-18 15:11:14 -0300

    plantilla detalle v3

--- app/Views/layout/mainDetail.php
+++ app/Views/layout/mainDetail.php
@@ -20,6 +20,55 @@
             
             <img src="<?=base_url();?>/assets/img/navegacion.svg" alt="">
         </div>
+        <div class="column is-mobile listDetail">
+            <label class="label is-large"><i class="fas fa-user-clock"></i> Horario de atencion</label>
+            <div class="table-container">
+                <table class="table">
+                  <thead>
+                  <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  </tr>
+                    <tr>
+                        <td>Lunes</td>
+                        <td>8:00 - 19:00</td>
+                        <td class="estadoAbierto">abierto</td>
+                    </tr>
+                    <tr>
+                        <td>Martes</td>
+                        <td>8:00 - 19:00</td>
+                        <td class="estadoHorario"></td>
+
+                    </tr>
+                    <tr>
+                        <td>Mierciles</td>
+                        <td>8:00 - 19:00</td>
+                        <td class="estadoHorario"></td>
+                    </tr>
+                    <tr>
+                        <td>Jueves</td>
+                        <td>8:00 - 19:00</td>
+                        <td class="estadoHorario"></td>
+                    </tr>
+                    <tr>
+                        <td>Viernes</td>
+                        <td>8:00 - 19:00</td>
+                        <td class="estadoHorario"></td>
+                    </tr>
+                    <tr>
+                        <td>Sábado</td>
+                        <td>8:00 - 14:00</td>
+                        <td class="estadoCerrado">Cerrado</td>
+                    </tr>
+                    <tr>
+                        <td>Domingo</td>
+                        <td>X</td>
+                        <td class="estadoHorario"></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
         <div class="column is-mobile imageDetail">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3332.7221206003655!2d-70.72229838480321!3d-33.352208780802265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c0cbabfa6ead%3A0x16536166b31157bc!2sEl%20Abeto%203178%2C%20Quilicura%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1608231699129!5m2!1ses!2scl" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
@@ -38,13 +87,12 @@
         </div>
 
         <div class="column is-half">
-                    <label class="label is-large"><i class="fas fa-concierge-bell"></i> Miselaneos</label>
+                    <label class="label is-large"><i class="fas fa-map-marked-alt"></i> Surcursales</label>
 
-        <ul class="">
-                <li class=""><i class="fas fa-random"></i></i> Productos nuevos.  </li>
-                <li class=""><i class="fas fa-random"></i></i> Productos usados. </li>
-                <li class=""><i class="fas fa-random"></i></i> Despacho a domicilio </i></li>
-                <li class=""><i class="fas fa-random"></i></i> Retiro en tienda</li>
+            <ul class="">
+                <li class=""><i class="fas fa-street-view"></i> El bosque: el bosque 123.  </li>
+                <li class=""><i class="fas fa-street-view"></i> Huechuraba: la caral 236. </li>
+ 
             </ul>
         </div>
     </div>
